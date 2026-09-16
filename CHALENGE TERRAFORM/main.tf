d68988f31f2f6799d70b7e64ee9b35f12ea8cd04
arturoalmar <a29310@svalero.com>
2026-09-16 08:19:58 +0200

    Add files via upload

---
+++ CHALENGE TERRAFORM/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "vpc_prueba" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "devops-vpc-prueba"
+  }
+}
+
